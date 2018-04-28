2f5f559d1f0e9597cf4f32896f41e53ded16d5bf
c0ldSpring <36082814+c0ldSpring@users.noreply.github.com>
2018-04-28 15:41:33 +0300

    bugfix --- ordinea actiunilor este acum cea corecta

--- langtons_ant/Main.cpp
+++ langtons_ant/Main.cpp
@@ -4,12 +4,13 @@
 #include <vector>
 #include <ctime>
 #define MAX 500
-#define S 4.f
+#define S 16.f
 using namespace std;
 using namespace sf;
 
 int WIDTH, HEIGHT, SPEED; // marimea ferestrei + fps
 int nAnts, c_box[MAX][MAX]; // nr de furnici, vectorul de casete (pt afisare)
+int t[MAX][MAX]; // teren folosit in acest frame
 enum dir { UP = 0, RIGHT, DOWN, LEFT }; // pt directiile de deplasare ale furnicilor
 struct ant
 {
@@ -23,14 +24,14 @@ void setup()
 	cout << "W: "; cin >> WIDTH;
 	cout << "H: "; cin >> HEIGHT;
 	cout << "S: "; cin >> SPEED;
-	cout << "N: "; cin >> nAnts; nAnts++;
+	cout << "N: "; cin >> nAnts;
 }
 
 void initGrid()
 {
 	for (int i = 1; i <= HEIGHT; i++)
 		for (int j = 1; j <= WIDTH; j++) c_box[i][j] = 0; // reinitializarea casetelor pt afisare
-	int aux = nAnts, i, j, _len = 0;
+	int aux = (nAnts + 1), i, j, _len = 0;
 	srand(time(NULL));
 	while (aux--)
 	{
@@ -65,38 +66,45 @@ void updateAnts()
 		i_ant = c_ants[i].i; j_ant = c_ants[i].j; // coordonatele furnicii actuale
 		if (inside(i_ant, j_ant))
 		{
-			if (c_box[i_ant][j_ant] == 0) c_box[i_ant][j_ant] = 1; // daca caseta pe care se afla furnica acum e alba -> va deveni colorata
-			else c_box[i_ant][j_ant] = 0; // altfel devine alba
+			if (c_box[i_ant][j_ant] == 0) t[i_ant][j_ant] = 1; // daca caseta pe care se afla furnica acum e alba -> va deveni colorata
+			else t[i_ant][j_ant] = 0; // altfel devine alba
 
 			switch (c_ants[i].d) // actualizam pozitia furnicii in functie de directie
 			{
 			case UP:
-				c_ants[i].i--; // furnica se deplaseaza cu o unitate mai sus
-				i_ant = c_ants[i].i; j_ant = c_ants[i].j; // retin pozitia actuala
 				if (c_box[i_ant][j_ant]) c_ants[i].d = LEFT; // caseta pe care a ajuns este colorata -> furnica face o intoarcere spre stanga
 				else c_ants[i].d = RIGHT; // altfel -> spre dreapta
+				if(c_ants[i].d==RIGHT) c_ants[i].j++;
+				else c_ants[i].j--;
 				break;
 			case RIGHT:
-				c_ants[i].j++; // furnica se deplaseaza cu o unitate in dreapta
-				i_ant = c_ants[i].i; j_ant = c_ants[i].j; // retin pozitia actuala
 				if (c_box[i_ant][j_ant]) c_ants[i].d = UP; // > intors la 90grade in stanga devine ^
 				else c_ants[i].d = DOWN; // > intors la 90grade in dreapta devine v
+				if(c_ants[i].d==DOWN) c_ants[i].i++;
+				else c_ants[i].i--;
 				break;
 			case DOWN:
-				c_ants[i].i++; // furnica se deplaseaza cu o unitate in jos
-				i_ant = c_ants[i].i; j_ant = c_ants[i].j; // retin pozitia actuala
 				if (c_box[i_ant][j_ant]) c_ants[i].d = RIGHT; // v intors la 90grade in stanga devine >
 				else c_ants[i].d = LEFT; // v intors la 90grade in dreapta devine <
+				if(c_ants[i].d==RIGHT) c_ants[i].j++;
+				else c_ants[i].j--;
 				break;
 			case LEFT:
-				c_ants[i].j--; // furnica se deplaseaza cu o unitate in stanga
-				i_ant = c_ants[i].i; j_ant = c_ants[i].j; // retin pozitia actuala
 				if (c_box[i_ant][j_ant]) c_ants[i].d = DOWN; // < intors la 90grade in stanga devine v
 				else c_ants[i].d = UP; // < intors la 90grade in dreapta devine ^
+				if(c_ants[i].d==DOWN) c_ants[i].i++;
+				else c_ants[i].i--;
 				break;
 			}
 		}
 	}
+}
+
+void newTerrain()
+{
+    // actualizeaza teren
+	for(int i=1;i<=HEIGHT;i++)
+        for(int j=1;j<=WIDTH;j++) c_box[i][j]=t[i][j];
 }
 
 void drawAnts(RenderWindow &window)
@@ -135,7 +143,7 @@ int main()
 
 		// logica
 		window.clear(Color::White);
-		updateAnts();
+		if(Keyboard::isKeyPressed(Keyboard::Space)) updateAnts();
 
 		// afisare
 		drawBoxes(window);
